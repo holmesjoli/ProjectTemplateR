1aa896f3164d2be79f09a00fbd1bff3c43114bdb
Joli Holmes <jh111@ad.rice.edu>
2019-05-09 12:29:56 -0500

    added create package line

--- R/main.R
+++ R/main.R
@@ -1,21 +1,26 @@
-source("R/helpers.R")
+proj_root <- file.path(normalizePath(dirname(".")), "ProjectTemplateR")
+
+source(file.path(proj_root, "R", "helper_functions.R"))
+source(file.path(proj_root, "R", "cli_args.R"))
 
 pkgs <- c("usethis", "packrat", "optparse")
 install_pkg_list(pkgs)
-
+ 
 opts <- parseArguments()
 
 if (!is.null(opts[["project_name"]])) {
-    usethis::proj_activate(file.path(opts[["project_name"]]))
+    pth <- file.path(getwd(), opts[["project_name"]])
+    usethis::create_package(pth)
+    usethis::proj_activate(pth)
     use_packrat()
+    usethis::use_roxygen_md()
     usethis::use_rcpp()
     usethis::use_readme_md()
-    usethis::use_roxygen_md()
     usethis::use_testthat()
     usethis::use_git()
-    
+
 } else {
     stop("Must specify project name")
 }
 
-utilsR::create_dirs("R")
+utilsR::create_dirs("R", "man")
